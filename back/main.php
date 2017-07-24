596614671c015dbf400dcdff0e9274fc83176205
李冬 <1136855714@qq.com>
2017-07-24 21:57:21 +0800

    test login 8

--- back/main.php
+++ back/main.php
@@ -12,7 +12,7 @@ $admin = false;
 // 启动会话，这步必不可少
 session_start();
 
-if(!isset($_SESSION['last_access']) || (time()-$_SESSION['last_access'])>6)
+if(!isset($_SESSION['last_access']) || (time()-$_SESSION['last_access'])>60)
 {
   unset($_SESSION['admin']);
 } else {
