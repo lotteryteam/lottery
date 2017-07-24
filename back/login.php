ad616eeb5c5e637df9d885fdccdbbcb2f1f23b4d
李冬 <1136855714@qq.com>
2017-07-24 21:18:38 +0800

    test login 4

--- back/login.php
+++ back/login.php
@@ -4,7 +4,13 @@ ini_set('display_errors', true);
 error_reporting(E_ALL);
 include 'conn.php';
 
+$username = $_GET['username'];
+$pass = $_GET['pass'];
 
-
-$sql = "select * from admin";
+$sql = "select * from admin where username = '$username' and password = '$pass'";
 $result = mysql_query($sql,$con);
+if (mysql_num_rows($result) == 0) {
+  echo "1";
+} else {
+  echo '0';
+}
--- front/js/scripts.js
+++ front/js/scripts.js
@@ -9,10 +9,22 @@ function login(){
         alert("账号和密码不为空！");
     }
 
-    // $.ajax({
-    //   type: 'GET',
-    //   url:
-    // })
+    $.ajax({
+      type: 'GET',
+      url: "../back/login.php",
+      data: {username: $username, pass: $pass},
+      dataType: 'text',
+      success:function(data){
+        alert(data);
+          console.log("++++++++++"+data);
+          // location.href="http://202.61.86.219/lottery/front/index.html";
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+          alert(XMLHttpRequest.status);
+          alert(XMLHttpRequest.readyState);
+          alert(textStatus);
+      }
+    })
 
     if(username == "admin" && pass=="123456"){
         $.ajax({
