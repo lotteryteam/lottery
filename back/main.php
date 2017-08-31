dc05217369cf1807e72ed6b4be4b07ac3ce82a4d
李冬 <1136855714@qq.com>
2017-09-01 00:29:05 +0800

    debug bootstrap table

--- back/main.php
+++ back/main.php
@@ -1,5 +1,6 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
 ini_set('display_errors', true);
 error_reporting(E_ALL);
 
@@ -36,38 +37,38 @@ include 'conn.php';
 echo "jjj";
 echo $_GET['limit'];
 
-if (isset($_GET["limit"]) && isset($_GET["offset"])) {
-  echo "kkk";
-  $limit = $_GET['limit'];
-  $offset = $_GET['offset'];
-  $start = $offset * $limit;
-  $sql = "select * from lottery limit $offset,$limit";
-  $sql_all_count = "select count(*) from lottery";
-  $result = mysql_query($sql, $con);
-  $array = new ArrayObject(new ArrayObject());
-  $i = 0;
-
-  echo $limit;
-  echo $offset;
-
-  while($row = mysql_fetch_array($result)){
-      $array[$i]["id"] = $row[0]; //id
-      $array[$i]["url"] = $row[1]; //url
-      $array[$i]["type"] = $row[2]; //type
-      $array[$i]["show_url"] = $row[3]; //show_url
-      $array[$i]["appid"] = $row[4]; //appid
-      $array[$i]["comment"] = $row[5]; //comment
-      $array[$i]["create"] = $row[6]; //create
-      $array[$i]["update"] = $row[7]; //update
-      $i++;
-  }
-
-  $result_count = mysql_query($sql_all_count, $con);
-  $row_count = mysql_fetch_array($result_count)
-  $all_count = $row_count[0];
-
-  $json = array("total" => $all_count, "rows" => $array);
-  echo json_encode($json)
-} else {
-   echo "111";
-}
+// if (isset($_GET["limit"]) && isset($_GET["offset"])) {
+//   echo "kkk";
+//   $limit = $_GET['limit'];
+//   $offset = $_GET['offset'];
+//   $start = $offset * $limit;
+//   $sql = "select * from lottery limit $offset,$limit";
+//   $sql_all_count = "select count(*) from lottery";
+//   $result = mysql_query($sql, $con);
+//   $array = new ArrayObject(new ArrayObject());
+//   $i = 0;
+//
+//   echo $limit;
+//   echo $offset;
+//
+//   while($row = mysql_fetch_array($result)){
+//       $array[$i]["id"] = $row[0]; //id
+//       $array[$i]["url"] = $row[1]; //url
+//       $array[$i]["type"] = $row[2]; //type
+//       $array[$i]["show_url"] = $row[3]; //show_url
+//       $array[$i]["appid"] = $row[4]; //appid
+//       $array[$i]["comment"] = $row[5]; //comment
+//       $array[$i]["create"] = $row[6]; //create
+//       $array[$i]["update"] = $row[7]; //update
+//       $i++;
+//   }
+//
+//   $result_count = mysql_query($sql_all_count, $con);
+//   $row_count = mysql_fetch_array($result_count)
+//   $all_count = $row_count[0];
+//
+//   $json = array("total" => $all_count, "rows" => $array);
+//   echo json_encode($json)
+// } else {
+//    echo "111";
+// }
