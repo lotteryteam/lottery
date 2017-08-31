aeff47aa3e9a6ab65c6e566deb8c62e760c78262
李冬 <1136855714@qq.com>
2017-09-01 01:24:00 +0800

    debug bootstrap table

--- back/main.php
+++ back/main.php
@@ -63,9 +63,9 @@ $row_count = mysql_fetch_row($result_count);
 echo $row_count[0];
 // $all_count = $row_count[0];
 //
-// $json = array("total" => $row_count[0], "rows" => $array);
-// echo json_encode($json)
-print_r($array);
+$json = array("total" => $row_count[0], "rows" => $array);
+echo json_encode($json);
+// print_r($json);
 
 // if (isset($_GET["limit"]) && isset($_GET["offset"])) {
 //   echo "kkk";
